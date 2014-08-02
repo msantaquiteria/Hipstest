85ef6008180593cb0ba5faead96047880dddba6b
Souler <soulerhyd@gmail.com>
2014-08-02 21:46:14 +0200

    api: Varios cambios: * Movidas las queries de SQL a un archivo independiente exclusivo para eso * Funcion de render generica movida a la raiz de API, en lugar de quiz * Entradas a la api para obtener el listado de asignaturas y la informacion de una en concreto

--- backend/router.js
+++ backend/router.js
@@ -2,13 +2,20 @@ var api = require('./api');
 
 module.exports = function(app) {
 	app.param('questionid', api.quiz.loadQuestion);
+	app.param('subjectid', api.quiz.loadSubject);
 
 	// #### API ####
 	// Single question
-	app.get('/api/question/random', api.quiz.randomQuestion, api.quiz.renderQuestion );
-	// Single question
-	app.get('/api/question/:questionid', api.quiz.renderQuestion );
-	// Fallback
+	app.get('/api/question/:questionid([0-9]+)', api.renderResult );
+	// Single random question (optionally filtered)
+	app.get('/api/question/random/:subjectid([0-9]+)?', api.quiz.randomQuestion, api.renderResult );
+
+	// Single subject
+	app.get('/api/subject/:subjectid([0-9]+)', api.renderResult );
+	// Subject unfiltered list
+	app.get('/api/subject', api.quiz.listSubjects, api.renderResult );
+
+	// API Fallback
 	app.get('/api/*', api.render404 );
 
 	app.get('/', function(req, res) { res.render('home.ejs'); });
