63955a45a88fb71b849385456a278a52af0578a4
Souler <soulerhyd@gmail.com>
2014-08-15 19:38:32 +0200

    www: corregidos templates con includes

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -3,15 +3,20 @@ var fs = require('fs');
 
 exports.render = {};
 exports.render.home = function(req, res, next) {
-	res.send(ejs.render(loadTemplate('home')));
+	res.send(render('home'));
 }
 
-function loadTemplate(path) {
+function render(path) {
 	var filePath = app.get('views').replace(/\/$/,'') + '/' + path.replace(/^\//, '') + '.ejs';
+	return ejs.render(loadTemplate(filePath), { filename: filePath });
+}
+
+function loadTemplate(filePath) {
 	var template = fs.readFileSync(filePath, 'utf8');
 
 	if ( app.get('url_append').length )
 		template = template.replace(/(src|href)=(['"])\//g, "$1=$2/" + app.get('url_append') + "/")
 
+	// template = template.replace(/[\t\r\n]/, '');
 	return template;
-}	
+}
